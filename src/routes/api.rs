2aa769212731cb17f6e49d7be3a90a738b67b29d
Woomymy <github@woomy.ovh>
2021-08-02 16:05:53 +0200

    Routes: Add /api/version

--- src/routes/api.rs
+++ src/routes/api.rs
@@ -3,6 +3,11 @@ use actix_web::{web, Responder};
 async fn ping() -> impl Responder {
     "ping"
 }
+async fn version() -> impl Responder {
+    env!("CARGO_PKG_VERSION")
+}
 pub fn api() -> actix_web::Scope {
-    web::scope("/api").route("/ping", web::get().to(ping))
+    web::scope("/api")
+        .route("/ping", web::get().to(ping))
+        .route("/version", web::get().to(version))
 }
