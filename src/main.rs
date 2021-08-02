7d4845223f2e1bb3f10eec29d6e4fba104776e3a
Woomymy <github@woomy.ovh>
2021-08-02 17:22:07 +0200

    Docker: Add docker-compose.dev.yml

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ async fn main() -> std::io::Result<()> {
             .service(api::api())
             .default_service(web::get().to(fof::fof))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
